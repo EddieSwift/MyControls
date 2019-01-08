ed7518f59c07bbd85220c51a7a2d6a80ada60010
EddieSwift <37484195+EddieSwift@users.noreply.github.com>
2019-01-08 17:12:58 +0200

    Initial Commit

---
+++ MyControls/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyControls
+//
+//  Created by Eduard Galchenko on 1/8/19.
+//  Copyright © 2019 Eduard Galchenko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MyControls/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyControls
+//
+//  Created by Eduard Galchenko on 1/8/19.
+//  Copyright © 2019 Eduard Galchenko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
